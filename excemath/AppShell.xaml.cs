2f01a7d713cf2fa47dd9b80aa9d019d070b23950
imlystyi <vladte3c@gmail.com>
2023-03-30 19:35:44 +0300

    Add project files.

---
+++ excemath/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace excemath;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ excemath/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace excemath;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
